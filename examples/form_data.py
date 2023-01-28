a7f299f55d87b6e94b2b5f3f79a8067595da08bf
aprilahijriyan <hijriyan23@gmail.com>
2023-01-28 12:25:42 +0700

    📃 docs: added sample project file upload and submit data form

---
+++ examples/form_data.py
@@ -0,0 +1,35 @@
+"""
+Form Data App
+=============
+
+A demo app for submitting form data and files.
+"""
+
+from allin import Allin, JSONResponse, status_codes
+from allin.errors import HTTPError
+from allin.globals import request
+from allin.params import UploadFile
+
+app = Allin()
+
+@app.post("/form")
+async def form_data():
+    if "application/x-www-form-urlencoded" in request.headers.content_type:
+        forms = await request.forms()
+        return JSONResponse(forms)
+    else:
+        raise HTTPError(status_codes.BAD_REQUEST, detail="Invalid request. Expected a form-urlencoded request")
+
+@app.post("/file")
+async def file_data():
+    if "multipart/form-data" in request.headers.content_type:
+        forms = await request.forms()
+        files = forms.get("files", [])
+        filenames = []
+        for file in files:
+            if isinstance(file, UploadFile):
+                filenames.append(file.filename)
+        detail = "Uploaded" if filenames else "No files uploaded"
+        return JSONResponse({"files": filenames, "detail": detail})
+    else:
+        raise HTTPError(status_codes.BAD_REQUEST, detail="Invalid request. Expected a multipart/form-data request")
